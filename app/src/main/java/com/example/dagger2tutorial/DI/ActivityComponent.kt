1b984d0fd49709a13e71cb4e99e713299d411bde
Ran Zeller <rzeller@chegg.com>
2019-09-25 18:15:21 +0300

    SubComponent

--- app/src/main/java/com/example/dagger2tutorial/DI/ActivityComponent.kt
+++ app/src/main/java/com/example/dagger2tutorial/DI/ActivityComponent.kt
@@ -3,25 +3,12 @@ package com.example.dagger2tutorial.DI
 import com.example.dagger2tutorial.MainActivity
 import dagger.BindsInstance
 import dagger.Component
+import dagger.Subcomponent
 import javax.inject.Named
 
 @PerActivity
-@Component(dependencies = [ApplicationComponent::class], modules = [WheelsModule::class, PetrolEngineModule::class])
+@Subcomponent(modules = [WheelsModule::class, ElectricEngineModule::class])
 interface ActivityComponent{
 
     fun inject(mainActivity: MainActivity)
-
-    @Component.Builder
-    interface Builder{
-
-        @BindsInstance
-        fun horsePower(@Named("horsePower") horsePower : Int) : Builder
-
-        @BindsInstance
-        fun engineCapacity(@Named("engineCapacity") engineCapacity : Int) : Builder
-
-        fun appComponent(applicationComponent: ApplicationComponent) : Builder
-
-        fun build() : ActivityComponent
-    }
 }
